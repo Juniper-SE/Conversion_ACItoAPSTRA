5a6c49ec64f0ac7716286c37ad55cc2c6de9d786
Adam Jarvis <ajarvis@apstra.com>
2024-06-07 17:15:45 +0100

    TF created - PyPi added

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="project_name",
-    version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    name="aci2apstra",
+    version="0.1.0",
+    author="Adam Jarvis",
+    author_email="ajarvis@juniper.net",
+    description="A tool for converting an ACI APIC configuration to an Apstra configuration, outputted as a Terraform configuration.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/iamjarvs/Conversion_ACItoAPSTRA",
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "jinja2",
+        # Add other dependencies here
     ],
     entry_points={
         'console_scripts': [
-            'project_name=main:main',
+            'aci_to_apstra=src.main:main',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
-
